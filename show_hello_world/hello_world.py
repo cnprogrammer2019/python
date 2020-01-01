5fbc35f2aa9e97c11e98c499acd95b54f066e5a3
cnprogrammer2019 <cnprogrammer@126.com>
2020-01-01 16:05:11 +0800

    从命令行获取数据

--- show_hello_world/hello_world.py
+++ show_hello_world/hello_world.py
@@ -10,9 +10,19 @@
 # @IDE: PyCharm
 
 
-def show_message(msg:str):
+import argparse
+
+
+def show_message(msg: str):
     print(msg)
 
 
 if __name__ == '__main__':
     show_message('Hello, 2020!')
+
+    args = argparse.ArgumentParser('输入参数')
+    args.add_argument('-n', '--name', help='你的姓名', required=True, type=str)
+    a = args.parse_args()
+    name = a.name
+    print(name)
+    show_message(name)
