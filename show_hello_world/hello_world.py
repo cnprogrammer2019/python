45cb24652ee7a94d300518adf128bf2caa0b8ad1
cnprogrammer2019 <cnprogrammer@126.com>
2020-01-01 15:46:29 +0800

    增加的测试

---
+++ show_hello_world/hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Time: 2020-01-01 15:43
+# @Software: python
+# @File: hello_world.py
+# @Author: Peter Ren
+# @Contact: cnprogrammer@126.com
+# @Site: 
+# @IDE: PyCharm
+
+
+def show_message(msg:str):
+    print(msg)
+
+
+if __name__ == '__main__':
+    show_message('Hello, 2020!')
